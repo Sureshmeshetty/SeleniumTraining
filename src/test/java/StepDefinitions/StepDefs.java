9171a053a9280de18f299fdcff3166e71c4fbd26
Suresh Thammishetty <suresh.thammishetti@gmail.com>
2025-01-28 23:03:02 +0530

    removed annotations
    re

--- src/test/java/StepDefinitions/StepDefs.java
+++ src/test/java/StepDefinitions/StepDefs.java
@@ -1,12 +1,12 @@
 package StepDefinitions;
 
 public class StepDefs {
-    @Override
+    
     public String toString() {
         return super.toString();
     }
 
-    @Override
+
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
